22139e33ecf673ca21fbd702ca7314b52ef4913d
Leza Producciones <leza.producciones.cine@gmail.com>
2026-07-27 19:11:55 -0300

    Configurar descarga permanente desde GitHub Releases

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ if (downloadBtn) {
     downloadBtn.addEventListener("click", () => {
 
         window.location.href =
-        "https://github.com/lezaproduccionescine-hash/leza-web/releases/download/v1.0.0/Leza.TV.Setup.1.0.0.exe";
+        "https://github.com/lezaproduccionescine-hash/leza-web/releases/latest/download/LezaTVSetup.exe";
 
     });
 
